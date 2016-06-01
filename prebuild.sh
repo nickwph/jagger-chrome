c2797e06ca8bcac7a1221381bbd62066aabd87a2
Nicholas Wong <nickwph@gmail.com>
2016-06-01 17:08:19 -0400

    Updated prebuild script.

--- prebuild.sh
+++ prebuild.sh
@@ -2,7 +2,12 @@
 
 # webpack-chrome-extension
 cp -r node_modules/WebpackChromeExtension/dev-env .
+
+# fix csp
 sed -i '' "s/localhost:3001/localhost:3001 https:\/\/cdn.firebase.com https:\/\/*.firebaseio.com/g" dev-env/manifest/processor/csp.js
+
+# enable scss and bind jquery
+sed -i '' "s/\/\/ 'scss|sass'/'scss|sass'/g" dev-env/webpack/config.js
 
 # codemirror
 mkdir -p prebuilds/codemirror
@@ -12,6 +17,3 @@ cp node_modules/codemirror/lib/codemirror.css prebuilds/codemirror/codemirror.cs
 mkdir -p prebuilds/bootstrap
 cp -r node_modules/bootstrap/dist/css prebuilds/bootstrap
 cp -r node_modules/bootstrap/dist/fonts prebuilds/bootstrap
-
-# enable scss
-sed -i '' "s/  \/\/ 'scss|sass': '\!sass-loader',/  'scss|sass': '\!sass-loader',/g" dev-env/webpack/config.js
